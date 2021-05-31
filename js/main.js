0d8e628ee363e2c4a9d1502bf7f56b69497e4b33
acdrDev <acdr260@gmail.com>
2021-05-31 11:45:26 -0500

    Added the function to choose how many words you want.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
 // Component selection
-let select = document.getElementById('select');
-let words = document.getElementById('words');
-let result = document.getElementById('result');
+let select = document.getElementById("select");
+let words = document.getElementById("words");
+let wordNumbersInput = document.getElementById("wordNumbers");
+let result = document.getElementById("result");
 
 // Event handler
-select.addEventListener('click', e => {
-    e.preventDefault();
+select.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    // Array with the words
-    let wordsSplit = words.value.split(',');
+  // Change the value of the input to a numerical value
+  let wordNumbers = parseInt(wordNumbersInput.value);
 
-    // Random location in the array
-    let word = Math.floor((Math.random() * (wordsSplit.length - 1 + 1) + 1)) - 1;
+  result.innerHTML = ""; // Reset values
 
-    // Insert the word with the random index into the HTML
-    result.innerText = wordsSplit[word];
-})
+  let wordsSplit = words.value.split(","); // Array with the words
+
+  if (isNaN(wordNumbers) || wordNumbers === 1) {
+    wordSelectorHandler(wordsSplit);
+  } else {
+    if (wordNumbers > wordsSplit.length) {
+      result.innerHTML =
+        "<p>La lista de palabras no puede ser menor al numero de palabras</p>";
+    } else {
+      for (let i = 0; i < wordNumbers; i++) {
+        wordSelectorHandler(wordsSplit);
+      }
+    }
+  }
+});
+
+// Receive an array of words
+function wordSelectorHandler(wordsSplit) {
+  // Random location in the array
+  let word = Math.floor(Math.random() * (wordsSplit.length - 1 + 1) + 1) - 1;
+
+  result.innerHTML += `<p>${wordsSplit[word]}</p>`;
+}
