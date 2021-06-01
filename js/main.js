28076537a6f6e8f3e950425cd4f3c6410b6e52ef
acdrDev <acdr260@gmail.com>
2021-05-31 20:53:06 -0500

    Separation options added

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 // Component selection
-let select = document.getElementById("select");
-let words = document.getElementById("words");
-let wordNumbersInput = document.getElementById("wordNumbers");
-let result = document.getElementById("result");
+let select = document.getElementById("select"); // Button to start
+let words = document.getElementById("words"); // Word list
+let wordNumbersInput = document.getElementById("wordNumbers"); // Input for the number of words
+let option = document.getElementById("option"); // Type of separation
+let result = document.getElementById("result"); // Label where the result will be displayed
 
 // Event handler
 select.addEventListener("click", (e) => {
   e.preventDefault();
-
+  
   // Change the value of the input to a numerical value
   let wordNumbers = parseInt(wordNumbersInput.value);
 
   result.innerHTML = ""; // Reset values
 
-  let wordsSplit = words.value.split(","); // Array with the words
+  let wordsSplit = wordSplitHandler(words.value, option.value);
 
   if (isNaN(wordNumbers) || wordNumbers === 1) {
     wordSelectorHandler(wordsSplit);
@@ -28,6 +29,26 @@ select.addEventListener("click", (e) => {
     }
   }
 });
+
+// Receive an word list and a separation option, is function retunrn a array with the words
+function wordSplitHandler(wordList, optionInput) {
+  let wordsSplit;
+
+  switch (optionInput) {
+    case "espacios":
+      wordsSplit = wordList.split(" ");
+      break;
+
+    case "comas":
+      wordsSplit = wordList.split(",");
+      break;
+
+    case "saltos":
+      wordsSplit = wordList.split(/\n/);
+  }
+
+  return wordsSplit;
+}
 
 // Receive an array of words
 function wordSelectorHandler(wordsSplit) {
